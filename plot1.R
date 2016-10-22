e3b9ff2ba82828ef56a5307fbd48de724214dd9b
RCheer3 <r.cheer3@gmail.com>
2016-10-22 15:16:36 -0700

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,15 @@
+quiz1 = read.table("exdata_data_household_power_consumption/household_power_consumption.txt",header = TRUE,sep = ';')
+getwd()
+
+quiz2 = subset(quiz1,Date == '2/1/2007'|Date=='2/2/2007')
+
+GlobalActivePower = as.numeric(as.character(quiz2$Global_active_power))
+hist(GlobalActivePower, col = 'red', xlab = "Global Active Power (kilowatts)",main = "Global Active Power")
+
+dev.copy(png,file = "plot1.png", width = 480,height = 480)
+dev.off()
+
+
+
+
+
